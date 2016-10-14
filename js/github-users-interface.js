25c5194a767a6d0d66c1e08336efaf9a23a6145a
Dean Scelza <deanscelza@gmail.com>
2016-10-14 15:16:05 -0700

    reworked js logic in 2nd attempt to call API

--- js/github-users-interface.js
+++ js/github-users-interface.js
@@ -1,14 +1,33 @@
+// var Profile = require('./../js/github-users.js').profileModule;
+// var apiKey = require('./../.env').apiKey;
+//
+// $(document).ready(function(){
+//   //event.preventDefault();
+//   $('#user').submit(function(event){
+//     var username = $('#username').val();
+//   });
+// });
+
+
+
+
+
+
+
+
 var Profile = require('./../js/github-users.js').profileModule;
-var apiKey = require('./../.env').apiKey;
 
+$(document).ready(function() {
+  var ourProfile = new Profile();
 
-$(document).ready(function(){
-  //event.preventDefault();
-  $('#user').submit(function(event){
-    var username = $('#username').val();
-   
+  $('#user').submit(function(event) {
+    event.preventDefault();
+    var searchInput = $('#username').val();
+    ourProfile.getProfile(searchInput);
+    // $('#info').html('...');
+    // setTimeout(function() {
+    //   $('#info').html(ourMovie.title + "<br>" + ourMovie.year + "<br>" + ourMovie.plot + "<img class='poster' src='" + ourMovie.poster + "'>");
+    // }, 1000);
   });
-  //response.forEach(function(profile) {
-    //$('#rpDisplay').append(profile.title + " <br> " + profile.description + " <p> ");
-  //});
+
 });
--- js/github-users.js
+++ js/github-users.js
@@ -1,24 +1,34 @@
+// function Profile() {
+//   this.name = null;
+//   this.title = null;
+//   this.desciption = null;
+// }
+//
+// Profile.prototype.getProfile = function(username){
+//   var currentProfile = this;
+//
+//   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+//     console.log(response);
+//   });
+// };
+//
+// exports.profileModule = Profile;
+
+
+
+
 function Profile() {
-  this.name = null;
-  this.title = null;
-  this.desciption = null;
+  this.username = null;
+  this.repoName = null;
+  this.repoDescription = null;
 }
 
-Profile.prototype.getProfile = function(username){
-  var currentProfile = this;
+Profile.prototype.getProfile = function(username) {
 
+  var currentProfile = this;
   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
     console.log(response);
   });
-
-  // exports.getRepos = function(){
-  //   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-  //     console.log(response);
-  //   }).fail(function(error){
-  //     console.log(error.responseJSON.message);
-  //   });
-  // };
-
 };
 
-exports.profileModule = Profile;
+exports.movieModule = Movie;
