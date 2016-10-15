eb0590426c2f5818a99eb29f249071c32863f386
Dean Scelza <deanscelza@gmail.com>
2016-10-15 10:40:53 -0700

    rework interface logic

--- js/github-users-interface.js
+++ js/github-users-interface.js
@@ -1,33 +1,37 @@
-// var Profile = require('./../js/github-users.js').profileModule;
-// var apiKey = require('./../.env').apiKey;
-//
-// $(document).ready(function(){
-//   //event.preventDefault();
-//   $('#user').submit(function(event){
-//     var username = $('#username').val();
-//   });
-// });
-
-
-
-
-
-
-
 
 var Profile = require('./../js/github-users.js').profileModule;
 
-$(document).ready(function() {
-  var ourProfile = new Profile();
+Profile.prototype.displayProfileInfo = function(){
+  $('#error').html(" ");
+  $('#userInfo').html(" ");
+  $('#userInfo').append("<img src='" + this.userInfo.avatar_url + "' class='avatar'>" + "<h3>" + this.userInfo.login + "</h3>" + "<p> GitHub Profile </p>" + "<p> Public Repos:  " + this.userInfo.public_repos + "</p>");
+};
 
-  $('#user').submit(function(event) {
-    event.preventDefault();
-    var searchInput = $('#username').val();
-    ourProfile.getProfile(searchInput);
-    // $('#info').html('...');
-    // setTimeout(function() {
-    //   $('#info').html(ourMovie.title + "<br>" + ourMovie.year + "<br>" + ourMovie.plot + "<img class='poster' src='" + ourMovie.poster + "'>");
-    // }, 1000);
+Profile.prototype.displayRepos = function(){
+  $('#repos').html(" ");
+  var repoDivs = "";
+  this.repos.forEach(function(repo){
+    repoDivs += "<div class='repoDisplay'>";
+    repoDivs += "<h4>" + repo.name + "</h4>";
+    repoDivs += "<p>" + repo.description + "</p>";
+    repoDivs += "</div>";
   });
+  $('#repos').html("<p> Recent Repos:  </p>" + repoDivs);
+};
 
+Profile.prototype.displayError = function(searchString){
+  $('#userInfo').html(" ");
+  $('#repos').html(" ");
+  var display = "<h3> Invalid GitHub UserName.  Retry if you insist. </h3>;"
+  $('#error').html(display);
+};
+
+$(document).ready(function(){
+  var currentProfile = new Profile();
+  $('#searchForm').submit(function(event){
+    event.preventDefault();
+    var searchInput = $('#usernameInput').val();
+    $('#usernameInput').val(" ");
+    currentProfile.getProfileInfo(searchInput);
+  });
 });
