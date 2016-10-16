589f25bef47068e5ec136f38dae9497530713796
Dean Scelza <deanscelza@gmail.com>
2016-10-16 15:03:00 -0700

    tweak output styling

--- js/github-users-interface.js
+++ js/github-users-interface.js
@@ -3,7 +3,7 @@ var Profile = require('./../js/github-users.js').profileModule;
 
 var showRepos = function(userData){
   userData.forEach(function(data){
-    $('#repos ol').append("<li>" + data.name + "<br>" + data.description + "<br></li>");
+    $('#repos ol').append("<li> Name:  " + data.name + "<br> Description:  " + data.description + "<br></li>");
   });
   $('#avatar').append("<img src='" + data.owner.avatar_url + "' alt='user avatar'>");
   $("#error").append(error.responseJSON.message);
@@ -12,8 +12,10 @@ var showRepos = function(userData){
 $(document).ready(function(){
   $('#userSearch').submit(function(event){
     event.preventDefault();
+    $('#repos ol').html(" ");
+    $('#handle').html(" ");
     var username = $("#usernameInput").val();
-    $("#handle").append("Username:  " + username + "<p><br><br> Recent Repository Names & Descriptions: ");
+    $("#handle").append("Username:  " + username + "<p><br><br> Recent Repos: ");
     var newProfile = new Profile(username);
     newProfile.getRepos(newProfile.username, showRepos);
   });
